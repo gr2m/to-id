95d91e42b95993c5d12a12aa00983aa7ae5245c6
Gregor Martynus <gregor@martynus.net>
2015-05-18 17:21:51 +0200

    fix: "-something funky-" ☛ "something-funky"

--- to-id.js
+++ to-id.js
@@ -9,10 +9,12 @@
   // We don't simply do a negative "/[^\w]+/" regex,
   // as it would exclude non-ascii letters, like "ä", "안", etc
   var nonLetters = /[ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~]+/g
+  var dashesAtBeginningOrEnd = /(^-+|-+$)/g
 
   function toId (string) {
     string = string.trim().toLowerCase()
     string = string.replace(nonLetters, '-')
+    string = string.replace(dashesAtBeginningOrEnd, '')
     return string
   }
 
